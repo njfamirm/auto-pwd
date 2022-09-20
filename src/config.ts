4a21e976707fe774ec637213b9f575a434ead698
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-09-20 12:39:03 +0430

    feat: use auto-pwd script

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+
+import {createLogger} from '@alwatr/logger';
+
+if (!(
+  process.env.ACCOUNT_ID !== undefined &&
+  process.env.ACCOUNT_PASSWORD !== undefined ||
+  process.env.ACCOUNT_COOKIE !== undefined
+)) {
+  throw new Error('Please set ACCOUNT_ID and ACCOUNT_PASSWORD or ACCOUNT_COOKIE');
+}
+
+export const config = {
+  name: process.env.NAME ?? 'PWDCrawler',
+  headless: Boolean(process.env.HEADLESS) ?? true,
+  devtool: Boolean(process.env.DEV_TOOL) ?? false,
+  account: {
+    id: process.env.ACCOUNT_ID,
+    password: process.env.ACCOUNT_PASSWORD,
+    cookie: process.env.ACCOUNT_COOKIE,
+  },
+};
+
+export const logger = createLogger(config.name);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,42 @@
-import {Browser} from './lab/browser.js';
+import {config, logger} from './config.js';
+import {PWDCrawler} from './lab/pwd-crawler.js';
+import {sleep} from './lab/utils.js';
 
-const browser = new Browser({
-  headless: false
+logger.logOther('!: Auto-PWD :!');
+
+const pwdBrowser = new PWDCrawler({
+  name: config.name,
+  headless: config.headless,
+  devtools: config.devtool,
 });
 
-await browser.readyStatePromise;
-const page = await browser.openPage('https://google.com');
-await page?.type(
-    'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input',
-    'puppeteer',
+await pwdBrowser.readyStatePromise;
+
+await pwdBrowser.loginWithCookie(
+    (config.account.cookie as string),
 );
-await page?.keyboard.press('Enter')
+
+// await pwdBrowser.loginWithPassword({
+//   id: (config.account.id as string),
+//   password: (config.account.password as string),
+// });
+
+while (!(await pwdBrowser.checkLoginStatus())) {
+  await sleep(3000);
+}
+
+const err = await pwdBrowser.startSession();
+if (err !== null) {
+  throw new Error(err.message);
+}
+
+await pwdBrowser.addNewInstance();
+
+await pwdBrowser.enterCommand(`
+echo $HOST
+curl -fsSL https://raw.githubusercontent.com/njfamirm/dotfiles/main/install.sh | bash
+`);
+
+// await pwdBrowser.close();
+
+logger.logOther('Done!');
